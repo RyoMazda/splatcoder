4ae56d1c19774b119c9afeef3f3200d7a34d6ad1
ryomazda <matsudaryojob@gmail.com>
2020-11-19 15:40:15 +0900

    add requirements

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
     include_package_data=True,
     python_requires='>=3.8',
     install_requires=[
+        'pyyaml',
+        'requests',
         'termcolor>=1.1.0',
         'jinja2>=1.0.0',
         'beautifulsoup4>=4.9.1',
